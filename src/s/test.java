e34d4935b197fa6c84f5b93ad8956d0433b1e97e
zainebkallel <kallelzaineb91@gmail.com>
2025-10-04 16:56:40 +0100

    first commit master

--- src/s/test.java
+++ src/s/test.java
@@ -3,8 +3,6 @@ package s;
 public class test {
   public static void main (String[] args) {
     System.out.println("Hello master");
-    System.out.println("C1 master");
-    System.out.println("C2 master");
-    System.out.println("C3 master");
+
 }
 }
