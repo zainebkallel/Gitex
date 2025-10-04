b8c9845a5c9f23a63c359b389e06442e74fc66c3
zainebkallel <kallelzaineb91@gmail.com>
2025-10-04 16:42:40 +0100

    master c2 commit

--- src/s/test.java
+++ src/s/test.java
@@ -4,6 +4,7 @@ public class test {
   public static void main (String[] args) {
     System.out.println("Hello master");
     System.out.println("C1 master");
+    System.out.println("C2 master");
 
 }
 }
