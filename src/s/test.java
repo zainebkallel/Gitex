9619ac39f83ccbb26a50af1680ebce538916cb44
zainebkallel <kallelzaineb91@gmail.com>
2025-10-16 11:32:37 +0100

    ajout du file jenkins

--- src/s/test.java
+++ src/s/test.java
@@ -3,8 +3,8 @@ package s;
 public class test {
   public static void main (String[] args) {
     System.out.println("Hello master");
-
-   /*lasst commit master*/
+    /*Hello feature1*/
+       /*lasst commit master*/
     
 }
  
