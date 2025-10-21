3268d79f55183c1945a7e1519315786e8377dc6a
zainebkallel <kallelzaineb91@gmail.com>
2025-10-21 11:40:55 +0100

    trigger pipeline

--- src/s/test.java
+++ src/s/test.java
@@ -3,8 +3,7 @@ package s;
 public class test {
   public static void main (String[] args) {
     System.out.println("Hello master");
-    /*Hello feature1*/
-       /*lasst commit master*/
+    
     
 }
 }
